8afbdeaa90a4228df04c6b02c32937b9c82e0b0a
NathanAguilar <nathan.rene.aguilar@gmail.com>
2023-04-18 22:47:30 -0700

    Chapters 06-15

---
+++ decisions.js
@@ -0,0 +1,40 @@
+/*
+var count = 3;
+if (count == 4){
+    console.log('Count is 4');
+} else if (count > 4){
+    console.log('Count is greater than 4')
+}else if (count < 4)
+    console.log('Count is less than four')
+else {
+    console.log('Count is NOT 4');
+}
+*/
+
+/*
+let hero = 'Batman';
+switch(hero.toLocaleLowerCase()) {
+    case 'superman':
+        console.log('super strength');
+        console.log('x-ray vison');
+    case 'batman':
+        console.log('intelligence');
+        console.log('fighting skills');
+        break;
+    default:
+        console.log('member of JLA');
+}
+*/
+
+// ternary operator 
+let a = 1, b = '1';
+// let result = (a === b) ? 'equal' : 'ineqal';
+// let result = (a === b) ? 'equal' : 'ineqal';
+let result = (a !== b) ? 'not equal' : 'not ineqal';
+console.log(result);
+// console.log((a == b) ? 'equal' : 'ineqal');
+
+
+
+
+
---
+++ expressions.js
@@ -0,0 +1,23 @@
+//This is bad:
+// a;
+
+//Type of expressions
+//=============================
+
+// 1. Variable Declartion
+// let a;
+
+// 2. Assign a value
+// a = 4;
+
+// Perform an evaluation that returns a single value
+// b + c
+
+let b = 3;
+let c = 2;
+// Three expressions in here... can you find them?
+let a = b + c;
+
+// 1. let a... variable declarition
+// 2. perform an eval b + c
+// 3. result assigned to a
