917c70da4e860bbbc1584c3b8e151055296ed958
NathanAguilar <nathan.rene.aguilar@gmail.com>
2023-04-27 19:11:46 -0700

    Ch.23 Constructor Function Calls with the new Keyword

---
+++ constructors.js
@@ -0,0 +1,30 @@
+/* let car = {
+    make: 'bmw',
+    model: '745li',
+    year: 2010,
+}; 
+*/
+
+/* function Car(make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+}
+
+let myCar = new Car('bmw', '745li' 2010);
+
+console.log(myCar); */
+
+function myFunction() {
+    console.log('I am a simple functions');
+}
+
+let myFunction = new MyFunction();
+
+console.log(typepf myFunction);
+
+// Can't really do anything with this particular
+// object .. its's certainly NOT a funciton
+// reference anymore!
+// myFunction();
+
